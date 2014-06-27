327226a381ab463fa7175fdc263c583446b6ae7e
Rohit Krishnan <rohitkrishnan@yahoo.com>
2014-06-26 21:01:04 -0700

    fixing

--- Assignment3/src/Menu.java
+++ Assignment3/src/Menu.java
@@ -2,10 +2,6 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Menu {
-
-<<<<<<< HEAD
-import java.awt.*; 
 public class Menu
 {
 	
@@ -36,7 +32,6 @@ public class Menu
 		//Left
 		//Browse
 		JPanel browse = new JPanel();
-		GridLayout gLayout = new GridLayout(2, 3);
 		myWindow.getContentPane().add(browse, BorderLayout.WEST);
 		JTextField field1 = new JTextField(10);
 		JButton browseBtn = new JButton("Browse");
@@ -47,10 +42,10 @@ public class Menu
 		browse.add(caption);
 		browse.add(field2);
 		
-//		//Save
-		JPanel save = new JPanel();
-		myWindow.getContentPane().add(save, BorderLayout.LINE_START);
-		
+  		//Save
+	JPanel save = new JPanel();
+		myWindow.getContentPane().add(save, BorderLayout.AFTER_LINE_ENDS);
+	
 		JButton saveBtn = new JButton("Save");
 		save.add(saveBtn);
 		
@@ -86,56 +81,6 @@ public class Menu
 		
 		
 		myWindow.setVisible(true); 
-		
 	}
-=======
-    public void showMenu() {
-        JFrame myWindow = new JFrame("Assignment 3");
-        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        myWindow.setSize(1000, 1000);
-
-        JMenuBar menubar = new JMenuBar();
-        JMenu n = new JMenu("New");
-        JMenu save = new JMenu("Save");
-        JMenu open = new JMenu("Open");
-        JMenu exit = new JMenu("Exit");
-        menubar.add(n);
-        menubar.add(save);
-        menubar.add(open);
-        menubar.add(exit);
-        
-        myWindow.setJMenuBar(menubar);
-
-
-
-        JPanel EditBar = new JPanel();
-        //EditBar.setPreferredSize(new Dimension(100,100));
-        //EditBar.setLayout(new BoxLayout(EditBar, BoxLayout.Y_AXIS));
-
-        //myWindow.getContentPane().add(EditBar, FlowLayout.LEFT);
-
-        //Browse File 
-        JLabel Search = new JLabel("Image: ");
-        JTextField SrchText = new JTextField(15);
-        JButton Browse = new JButton("Browse");
-        EditBar.add(Search);
-        EditBar.add(SrchText);
-        EditBar.add(Browse);
-        //Caption
-        JLabel Caption = new JLabel("Caption: ");
-        JTextField c = new JTextField(15);
-        EditBar.add(Caption);
-        EditBar.add(c);
-        myWindow.getContentPane().add(EditBar, BorderLayout.WEST);
-
-        JButton saveCaption = new JButton("Save");
-        EditBar.add(saveCaption, "Save");
-
-
-
-
-        myWindow.setVisible(true);
-
-    }
->>>>>>> d3c43f2da153c5d1998ade7f299bf7b99a79566e
 }
+  
