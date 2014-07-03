8bba46ab18fcd622f335a2c9e497f8fd00b67424
Rohit Krishnan <rohitkrishnan@yahoo.com>
2014-07-02 22:36:54 -0700

    added image viewing code framework

--- Assignment3/src/Image.java
+++ Assignment3/src/Image.java
@@ -1,5 +1,33 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import java.net.URL;
+import javax.imageio.*;
 
-public class Image {
+
+public class Image extends Applet{
+    
+	/*
+	 * Code to take an image and view it
+	 */
+	private BufferedImage img;
+    
+    public void init() {
+        try {
+            URL url = new URL(getCodeBase(), "examples/strawberry.jpg");
+            img = ImageIO.read(url);
+        } catch (IOException e) {
+        }
+    }
+
+    public void paint(Graphics g) {
+      g.drawImage(img, 50, 50, null);
+    }
+
+	
 	public void uploadImage(){
 		
 	}
