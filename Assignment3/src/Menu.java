c5479c9b7e546bdf3396e0533455e557257b72e7
Brian Lee <maelstromfire@yahoo.com>
2014-07-02 21:23:10 -0700

    Fixed the GUI, Cleaned up the Code
    TODO:
    -Add Event Listeners
    -Add Photo Viewer for Right Side Panel

--- Assignment3/src/Menu.java
+++ Assignment3/src/Menu.java
@@ -5,117 +5,213 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Menu
-{
-	public JPanel photo = new JPanel();	
-	public JFrame myWindow = new JFrame("Assignment 3");
-	public Events myEvents = new Events(); 
-	public ControlMenu myControls = new ControlMenu(); 
-	public void showMenu()
-	{
-		//File (Top)
-		myEvents.theEvents();
-		myControls.Control();
-		
-		myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		myWindow.setSize(1000,1000); 
-		myWindow.setJMenuBar(myControls.getJMenu());
-		
-		
-		//Control Bar is the left panel that has all our controls for the program 
-		JPanel ControlBar = new JPanel();
-		ControlBar.setLayout(new BoxLayout(ControlBar, BoxLayout.Y_AXIS));
-		//put the image field and browse button in a panel to add to the control bar
-		
-		JPanel imagePanel = new JPanel();
-		imagePanel.setLayout(new BoxLayout(imagePanel, BoxLayout.X_AXIS));
-		//contents of image panel, which consist of a textfield and browse button i hard coded the size right now since it wont show properly 
-		JTextField imageTxt = new JTextField();
-		imageTxt.setPreferredSize(new Dimension(200, 1));
-		JButton browseBtn = new JButton();
-		browseBtn.setText("Browse");
-		//added browse and text field to the image panel 
-		imagePanel.add(new JLabel("Image:"));
-		imagePanel.add(imageTxt);
-		imagePanel.add(browseBtn);
-		//added image panel to the control bar
-		ControlBar.add(imagePanel);
-		//Use RigidAreas instead of struts. 
-		ControlBar.add(Box.createRigidArea(new Dimension(10, 10))); 
-		
-		//caption panel where we have the text field for the caption 
-		JPanel CaptionPanel = new JPanel();
-		CaptionPanel.setLayout(new BoxLayout(CaptionPanel, BoxLayout.X_AXIS));
-		JTextField captionField = new JTextField();
-		 //captionField.setPreferredSize(new Dimension(250, 1));
-		//added caption label and the captionfield to the caption panel 
-		CaptionPanel.add(new JLabel("Caption:"));
-		CaptionPanel.add(captionField);
-		//added the caption part to the control panel 
-		ControlBar.add(CaptionPanel);
-		ControlBar.add(Box.createRigidArea(new Dimension(10, 10))); 
-		
+public class Menu {
 
-		
-		//save button is supposed to go under the caption bar so i made a new panel 
-		JPanel saveBar = new JPanel();
-		saveBar.setLayout(new BoxLayout(saveBar, BoxLayout.X_AXIS));
-		JButton saveBtn = new JButton("Save");
-		//setMaximumSize for the button 
-		saveBtn.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
-		saveBar.add(saveBtn);
-		ControlBar.add(saveBar);
-		ControlBar.add(Box.createRigidArea(new Dimension(10, 10))); 
-		
-	
-		myWindow.add(ControlBar, BorderLayout.WEST);
-		
-		
-		//Save button
-		
+    public JPanel photo = new JPanel();
+    public JFrame myWindow = new JFrame("Assignment 3");
+    public Events myEvents = new Events();
+    public ControlMenu myControls = new ControlMenu();
 
-		browseBtn.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
+    public void showMenu() {
+        //JFrame Properties
+        myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myWindow.setSize(800, 600);
+        myWindow.setVisible(true);
+        myWindow.setJMenuBar(myControls.getJMenu());
 
-				int OpenDialog = myEvents.getChooser().showOpenDialog(myWindow);
-			}
-		});
-	
-		//Photo List
-		
-		//What does this do? Does not make sense. - Kareem 
-		myWindow.getContentPane().add(photo, BorderLayout.PAGE_END);
-		JLabel image1 = new  JLabel("imagex");
-		JLabel image2 = new JLabel("imagex");
-		JLabel image3 = new JLabel("imagex");
-		photo.add(image1);
-		photo.add(image2);
-		photo.add(image3);
-		
-		
-		JButton add = new JButton("Add New");
-		photo.add(add);	
-			class action implements ActionListener{
-				@Override
-				public void actionPerformed(ActionEvent arg0){
-						JButton newButton = new JButton("new image");
-						photo.add(newButton);
-					
-				}
-			}
-		add.addActionListener(new action());	
-		
-		//Area that shows the actual selected photo
+        //Menubar (File)
+        myEvents.theEvents();
+        myControls.Control();
+
+
+        /*
+         * LEFT SIDE PANEL
+         * ControlBar is the left panel that has all our controls for the program 
+         * Contains an INNER PANEL for each row of components
+         */
+        JPanel ControlBar = new JPanel();
+        ControlBar.setLayout(new BoxLayout(ControlBar, BoxLayout.Y_AXIS));
+        myWindow.add(ControlBar, BorderLayout.WEST);
+
+
+        /*
+         * INNER PANEL imagePanel
+         * imagePanel is the JPanel for holding Label, TextField, and Button
+         */
+        JPanel imagePanel = new JPanel();
+        JLabel imageLabel = new JLabel("  Image: ");
+        JTextField imageText = new JTextField(11);
+        JButton browseButton = new JButton("Browse");
+        browseButton.setPreferredSize(new Dimension(120, 24));
+
+        //Add Label, TextField, and Button to Panel
+        imagePanel.add(imageLabel);
+        imagePanel.add(imageText);
+        imagePanel.add(browseButton);
+
+        imagePanel.setPreferredSize(new Dimension(300, 20));
+        //Add imagePanel to the ControlBar
+        ControlBar.add(imagePanel);
+
+
+
+        /*
+         * INNER PANEL CaptionPanel
+         * CaptionPanel is the JPanel for holding Label and TextField
+         * 
+         */
+        JPanel captionPanel = new JPanel();
+        JLabel captionLabel = new JLabel("Caption: ");
+        JTextField captionField = new JTextField(22);
+
+        //Add Label and TextField to Panel
+        captionPanel.add(captionLabel);
+        captionPanel.add(captionField);
+
+        captionPanel.setPreferredSize(new Dimension(300, 20));
+        //Add captionPanel to the ControlBar
+        ControlBar.add(captionPanel);
+
+
+
+        /*
+         * INNER PANEL savePanel
+         * savePanel is the JPanel for holding Label and TextField
+         * 
+         */
+        JPanel savePanel = new JPanel();
+        JButton saveButton = new JButton("Save");
+        saveButton.setPreferredSize(new Dimension(300, 40));
+        
+        savePanel.setPreferredSize(new Dimension(300, 40));
+        //Add Button to Panel
+        savePanel.add(saveButton);
+
+        //Add Panel to ControlBar
+        ControlBar.add(savePanel);
+
+
+        
+        /*
+         * INNER PANEL photoListPanel
+         * photoListPanel is the JPanel for holding List and ScrollPane
+         */
+        JPanel photoListPanel = new JPanel();
+        DefaultListModel listModel = new DefaultListModel();
+        
+        //JList and Properties
+        JList photolist = new JList(listModel);
+        photolist.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        photolist.setSelectedIndex(0);
+        photolist.setVisibleRowCount(5);
+        //photolist.addListSelectionListener(this);
+        
+        JScrollPane listScrollPane = new JScrollPane(photolist);
+        listScrollPane.setPreferredSize(new Dimension(300, 350));
+        //Delete when not needed
+        listModel.addElement("Image 1");
+        listModel.addElement("Image 2");
+        listModel.addElement("Image 3");
+        
+        //Add ScrollPane to Panel
+        photoListPanel.add(listScrollPane);
+        
+        //Add Panel to ControlBar
+        ControlBar.add(photoListPanel);
+        
+        //What Component can create a List for the photos? - Brian
+
+
+
+        /*
+         * INNER PANEL AddNewButtonPanel
+         * addNewButtonPanel is the JPanel for holding Button
+         * 
+         */
+        JPanel addNewButtonPanel = new JPanel();
+        JButton addNewButton = new JButton("Add New");
+
+        //Add Button to Panel
+        addNewButtonPanel.add(addNewButton);
+        addNewButton.setPreferredSize(new Dimension(300, 40));
+        
+        addNewButtonPanel.setPreferredSize(new Dimension(300, 40));
+        //Add Panel to ControlBar
+        ControlBar.add(addNewButtonPanel);
+        
+        
+        //What does this do? Does not make sense. - Kareem 
+        //Was this supposed to be a part of the viewer of photos to display? - Brian
+/*
+        myWindow.getContentPane().add(photo, BorderLayout.PAGE_END);
+        JLabel image1 = new JLabel("imagex");
+        JLabel image2 = new JLabel("imagex");
+        JLabel image3 = new JLabel("imagex");
+        photo.add(image1);
+        photo.add(image2);
+        photo.add(image3);
+
+
+
+        photo.add(addNew);
+        class action implements ActionListener {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                JButton newButton = new JButton("new image");
+                photo.add(newButton);
+
+            }
+        }
+        addNew.addActionListener(new action());
+*/
+
+        /*
+         * EVENTS (Buttons)
+         * This area will contain the methods for each button:
+         * -Browse
+         * -Save
+         * -Add New
+         */
+
+        /*
+         * Browse Button
+         * 
+         * 
+         */
+        browseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                int OpenDialog = myEvents.getChooser().showOpenDialog(myWindow);
+            }
+        });
+
+
+        /*
+         * Save button
+         * Need to Implement Save Button
+         * 
+         */
+
+        /*
+         * Add New button
+         * Need to Implement Add New Button
+         * See Above where one is added.
+         */
+
+        //~~~~~~~~~~~~~~~~~~~End EVENTS
+
+
+
+        //Area that shows the actual selected photo
 //		JPanel area = new JPanel();
 //		myWindow.getContentPane().add(area, BorderLayout.EAST);
-	//	JImageComponent = new JImageComponent(myImageGoesHere);
-		myWindow.setVisible(true); 
-	}
-	
-	public JFrame getWindow()
-	{
-		return myWindow; 
-	}
+        //	JImageComponent = new JImageComponent(myImageGoesHere);
+        
+    }
+
+    public JFrame getWindow() {
+        return myWindow;
+    }
 }
-  
